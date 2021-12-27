2b9a9aa6aa59f4a6d1b4ada573d553b224b74f91
Dmytro Lapko <dlk@mercell.com>
2021-12-27 17:11:43 +0200

    Rename vars by GoLand strange recommendations, remove unneeded cast, add ignore spell check for java class names

--- filter/filter_test.go
+++ filter/filter_test.go
@@ -5,55 +5,56 @@ import (
 	"testing"
 )
 
-type v struct {
+type VersionVulnerability struct {
 	version    string
 	vulnerable bool
 }
 
 func TestFilterJndi(t *testing.T) {
-	for _, cand := range []v{
-		v{"2.1", true},
-		v{"2.2", true},
-		v{"2.3", true},
-		v{"2.4", true},
-		v{"2.4.1", true},
-		v{"2.5", true},
-		v{"2.6", true},
-		v{"2.6.1", true},
-		v{"2.6.2", true},
-		v{"2.7", true},
-		v{"2.8", true},
-		v{"2.8.1", true},
-		v{"2.8.2", true},
-		v{"2.9.0", true},
-		v{"2.9.1", true},
-		v{"2.10.0", true},
-		v{"2.11.0", true},
-		v{"2.11.1", true},
-		v{"2.11.2", true},
-		v{"2.12.0", true},
-		v{"2.12.1", true},
-		v{"2.12.2", false},
-		v{"2.13.0", true},
-		v{"2.13.1", true},
-		v{"2.13.2", true},
-		v{"2.13.3", true},
-		v{"2.13-3-debian", true},
-		v{"2.14.0", true},
-		v{"2.14.1", true},
-		v{"2.15.0", true},
-		v{"2.16.0", false},
-		v{"2.16.0-debian", false},
-		v{"2.17.0", false},
+	for _, item := range []VersionVulnerability{
+		{"2.1", true},
+		{"2.2", true},
+		{"2.3", true},
+		{"2.4", true},
+		{"2.4.1", true},
+		{"2.5", true},
+		{"2.6", true},
+		{"2.6.1", true},
+		{"2.6.2", true},
+		{"2.7", true},
+		{"2.8", true},
+		{"2.8.1", true},
+		{"2.8.2", true},
+		{"2.9.0", true},
+		{"2.9.1", true},
+		{"2.10.0", true},
+		{"2.11.0", true},
+		{"2.11.1", true},
+		{"2.11.2", true},
+		{"2.12.0", true},
+		{"2.12.1", true},
+		{"2.12.2", false},
+		{"2.13.0", true},
+		{"2.13.1", true},
+		{"2.13.2", true},
+		{"2.13.3", true},
+		{"2.13-3-debian", true},
+		{"2.14.0", true},
+		{"2.14.1", true},
+		{"2.15.0", true},
+		{"2.16.0", false},
+		{"2.16.0-debian", false},
+		{"2.17.0", false},
 	} {
-		file := "../testdata/JndiManager.class-" + cand.version
+		file := "../testdata/JndiManager.class-" + item.version
 		buf, err := ioutil.ReadFile(file)
 		if err != nil {
 			t.Logf("can't open %s: %v", file, err)
 			continue
 		}
-		if verdict := IsVulnerableClass(buf, "jndimanager.class", true); (verdict != "") != cand.vulnerable {
-			if cand.vulnerable {
+		//goland:noinspection SpellCheckingInspection
+		if verdict := IsVulnerableClass(buf, "jndimanager.class", true); (verdict != "") != item.vulnerable {
+			if item.vulnerable {
 				t.Errorf("found %s not to be vulnerable (but it is)", file)
 			} else {
 				t.Errorf("found %s to be vulnerable (but it is not)", file)
@@ -65,30 +66,31 @@ func TestFilterJndi(t *testing.T) {
 }
 
 func TestFilterSocketNode(t *testing.T) {
-	for _, cand := range []v{
-		v{"1.2.4", true},
-		v{"1.2.5", true},
-		v{"1.2.6", true},
-		v{"1.2.7", true},
-		v{"1.2.8", true},
-		v{"1.2.9", true},
-		v{"1.2.11", true},
-		v{"1.2.12", true},
-		v{"1.2.13", true},
-		v{"1.2.14", true},
-		v{"1.2.15", true},
-		v{"1.2.16", true},
-		v{"1.2.17", true},
-		v{"1.2.17-debian", false},
+	for _, item := range []VersionVulnerability{
+		{"1.2.4", true},
+		{"1.2.5", true},
+		{"1.2.6", true},
+		{"1.2.7", true},
+		{"1.2.8", true},
+		{"1.2.9", true},
+		{"1.2.11", true},
+		{"1.2.12", true},
+		{"1.2.13", true},
+		{"1.2.14", true},
+		{"1.2.15", true},
+		{"1.2.16", true},
+		{"1.2.17", true},
+		{"1.2.17-debian", false},
 	} {
-		file := "../testdata/SocketNode.class-" + cand.version
+		file := "../testdata/SocketNode.class-" + item.version
 		buf, err := ioutil.ReadFile(file)
 		if err != nil {
 			t.Logf("can't open %s: %v", file, err)
 			continue
 		}
-		if verdict := IsVulnerableClass(buf, "socketnode.class", true); (verdict != "") != cand.vulnerable {
-			if cand.vulnerable {
+		//goland:noinspection SpellCheckingInspection
+		if verdict := IsVulnerableClass(buf, "socketnode.class", true); (verdict != "") != item.vulnerable {
+			if item.vulnerable {
 				t.Errorf("found %s not to be vulnerable (but it is)", file)
 			} else {
 				t.Errorf("found %s to be vulnerable (but it is not)", file)
