f5a0c22a234f76bca809df59b3df192246348d9d
Dmytro Lapko <dlk@mercell.com>
2021-12-27 17:19:54 +0200

    Try to tell GoLand that it is ok not to check for Fprintf results, add alias for filter for visibility, fix mistypings

--- scanner/main.go
+++ scanner/main.go
@@ -11,7 +11,7 @@ import (
 	"strings"
 
 	zip "github.com/hillu/local-log4j-vuln-scanner/appendedzip"
-	"github.com/hillu/local-log4j-vuln-scanner/filter"
+	detectorFilter "github.com/hillu/local-log4j-vuln-scanner/filter"
 )
 
 var logFile = os.Stdout
@@ -19,11 +19,11 @@ var errFile = os.Stderr
 
 func handleJar(path string, ra io.ReaderAt, sz int64) {
 	if verbose {
-		fmt.Fprintf(logFile, "Inspecting %s...\n", path)
+		_, _ = fmt.Fprintf(logFile, "Inspecting %s...\n", path)
 	}
 	zr, err := zip.NewReader(ra, sz)
 	if err != nil {
-		fmt.Fprintf(logFile, "cant't open JAR file: %s (size %d): %v\n", path, sz, err)
+		_, _ = fmt.Fprintf(logFile, "cant't open JAR file: %s (size %d): %v\n", path, sz, err)
 		return
 	}
 	for _, file := range zr.File {
@@ -34,42 +34,42 @@ func handleJar(path string, ra io.ReaderAt, sz int64) {
 		case ".jar", ".war", ".ear":
 			fr, err := file.Open()
 			if err != nil {
-				fmt.Fprintf(logFile, "can't open JAR file member for reading: %s (%s): %v\n", path, file.Name, err)
+				_, _ = fmt.Fprintf(logFile, "can't open JAR file member for reading: %s (%s): %v\n", path, file.Name, err)
 				continue
 			}
 			buf, err := ioutil.ReadAll(fr)
-			fr.Close()
+			_ = fr.Close()
 			if err != nil {
-				fmt.Fprintf(logFile, "can't read JAR file member: %s (%s): %v\n", path, file.Name, err)
+				_, _ = fmt.Fprintf(logFile, "can't read JAR file member: %s (%s): %v\n", path, file.Name, err)
 			}
 			handleJar(path+"::"+file.Name, bytes.NewReader(buf), int64(len(buf)))
 		default:
 			fr, err := file.Open()
 			if err != nil {
-				fmt.Fprintf(logFile, "can't open JAR file member for reading: %s (%s): %v\n", path, file.Name, err)
+				_, _ = fmt.Fprintf(logFile, "can't open JAR file member for reading: %s (%s): %v\n", path, file.Name, err)
 				continue
 			}
 
-			// Identify class filess by magic bytes
+			// Identify class files by magic bytes
 			buf := bytes.NewBuffer(nil)
 			if _, err := io.CopyN(buf, fr, 4); err != nil {
 				if err != io.EOF && !quiet {
-					fmt.Fprintf(logFile, "can't read magic from JAR file member: %s (%s): %v\n", path, file.Name, err)
+					_, _ = fmt.Fprintf(logFile, "can't read magic from JAR file member: %s (%s): %v\n", path, file.Name, err)
 				}
-				fr.Close()
+				_ = fr.Close()
 				continue
 			} else if !bytes.Equal(buf.Bytes(), []byte{0xca, 0xfe, 0xba, 0xbe}) {
-				fr.Close()
+				_ = fr.Close()
 				continue
 			}
 			_, err = io.Copy(buf, fr)
-			fr.Close()
+			_ = fr.Close()
 			if err != nil {
-				fmt.Fprintf(logFile, "can't read JAR file member: %s (%s): %v\n", path, file.Name, err)
+				_, _ = fmt.Fprintf(logFile, "can't read JAR file member: %s (%s): %v\n", path, file.Name, err)
 				continue
 			}
-			if desc := filter.IsVulnerableClass(buf.Bytes(), file.Name, !ignoreV1); desc != "" {
-				fmt.Fprintf(logFile, "indicator for vulnerable component found in %s (%s): %s\n", path, file.Name, desc)
+			if desc := detectorFilter.IsVulnerableClass(buf.Bytes(), file.Name, !ignoreV1); desc != "" {
+				_, _ = fmt.Fprintf(logFile, "indicator for vulnerable component found in %s (%s): %s\n", path, file.Name, desc)
 				continue
 			}
 		}
@@ -106,12 +106,12 @@ func main() {
 	flag.Var(&excludes, "exclude", "paths to exclude (can be used multiple times)")
 	flag.BoolVar(&verbose, "verbose", false, "log every archive file considered")
 	flag.StringVar(&logFileName, "log", "", "log file to write output to")
-	flag.BoolVar(&quiet, "quiet", false, "no ouput unless vulnerable")
+	flag.BoolVar(&quiet, "quiet", false, "no output unless vulnerable")
 	flag.BoolVar(&ignoreV1, "ignore-v1", false, "ignore log4j 1.x versions")
 	flag.Usage = func() {
-		fmt.Fprintf(flag.CommandLine.Output(), "Usage of %s:\n", os.Args[0])
+		_, _ = fmt.Fprintf(flag.CommandLine.Output(), "Usage of %s:\n", os.Args[0])
 		flag.PrintDefaults()
-		fmt.Fprint(flag.CommandLine.Output(), "  PATH [, PATH ...]\n        paths to search for Java code\n")
+		_, _ = fmt.Fprint(flag.CommandLine.Output(), "  PATH [, PATH ...]\n        paths to search for Java code\n")
 	}
 	flag.Parse()
 	args := flag.Args()
@@ -127,7 +127,7 @@ func main() {
 	if logFileName != "" {
 		f, err := os.Create(logFileName)
 		if err != nil {
-			fmt.Fprintln(os.Stderr, "Could not create log file")
+			_, _ = fmt.Fprintln(os.Stderr, "Could not create log file")
 			os.Exit(2)
 		}
 		logFile = f
@@ -136,9 +136,9 @@ func main() {
 	}
 
 	for _, root := range args {
-		filepath.Walk(filepath.Clean(root), func(path string, info os.FileInfo, err error) error {
+		_ = filepath.Walk(filepath.Clean(root), func(path string, info os.FileInfo, err error) error {
 			if err != nil {
-				fmt.Fprintf(errFile, "%s: %s\n", path, err)
+				_, _ = fmt.Fprintf(errFile, "%s: %s\n", path, err)
 				return nil
 			}
 			if excludes.Has(path) {
@@ -151,17 +151,17 @@ func main() {
 			case ".jar", ".war", ".ear":
 				f, err := os.Open(path)
 				if err != nil {
-					fmt.Fprintf(errFile, "can't open %s: %v\n", path, err)
+					_, _ = fmt.Fprintf(errFile, "can't open %s: %v\n", path, err)
 					return nil
 				}
 				defer f.Close()
-				sz, err := f.Seek(0, os.SEEK_END)
+				sz, err := f.Seek(0, io.SeekEnd)
 				if err != nil {
-					fmt.Fprintf(errFile, "can't seek in %s: %v\n", path, err)
+					_, _ = fmt.Fprintf(errFile, "can't seek in %s: %v\n", path, err)
 					return nil
 				}
-				if _, err := f.Seek(0, os.SEEK_END); err != nil {
-					fmt.Fprintf(errFile, "can't seek in %s: %v\n", path, err)
+				if _, err := f.Seek(0, io.SeekEnd); err != nil {
+					_, _ = fmt.Fprintf(errFile, "can't seek in %s: %v\n", path, err)
 					return nil
 				}
 				handleJar(path, f, sz)
